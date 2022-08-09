cd4d39de03efa4de5947e96154931a8a345ebb38
xiejon <jxie77@gmail.com>
2022-08-09 15:42:20 -0400

    added browser routing

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Homepage from './pages/Homepage'
+import Store from './pages/Store'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
     </div>
   );
 }
