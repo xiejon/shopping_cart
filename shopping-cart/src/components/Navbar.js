9c43603e8400f86b7043b2fcab27c793f05f6460
xiejon <jxie77@gmail.com>
2022-08-10 20:26:50 -0400

    added cart quantity to ccart svg

--- shopping-cart/src/components/Navbar.js
+++ shopping-cart/src/components/Navbar.js
@@ -10,8 +10,26 @@ const Navbar = () => {
             </div>
             <div className={styles.links}>
                 <Link className={styles.link} to="/">Home</Link>
-                <Link className={styles.link} to="/store">Store</Link> 
-                <svg className={styles.cart} xmlns="http://www.w3.org/2000/svg" height="63" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+                <Link className={styles.link} to="/store">Store</Link>
+                <button className={styles.cartBtn}>
+                    <svg 
+                        className={styles.cart} 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        height="63" 
+                        viewBox="0 0 24 24" 
+                        fill="none" 
+                        stroke="black" 
+                        stroke-width="2" 
+                        stroke-linecap="round" 
+                        stroke-linejoin="round" 
+                        class="feather feather-shopping-cart">
+                        <circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle>
+                        <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                    </svg>
+                    <div className={styles.quantity}>
+                        3
+                    </div>
+                </button> 
             </div>
         </div>
     )
