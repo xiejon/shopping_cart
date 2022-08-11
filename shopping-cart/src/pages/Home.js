403979ce36f913c7aa90154bf5d76c5db2b6aee7
xiejon <jxie77@gmail.com>
2022-08-10 20:10:27 -0400

    restyled homepage

--- shopping-cart/src/pages/Home.js
+++ shopping-cart/src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
     return(
         <div className={styles.container}>
-            <img className={styles.bg} src={require('../resources/images/home-bg.jpg')} alt="tea"></img>     
+            <div className={styles.main}>
+                <h3 className={styles.quote}><em>'A BATH REFRESHES THE BODY, TEA REFRESHES THE MIND.'</em></h3>
+                <button className={styles.btn} type="button">
+                    <Link className={styles.link} to='/store'>VISIT THE STORE</Link>
+                </button>
+            </div>
+            <img className={styles.bg} src={require('../resources/images/home-bg.jpg')} alt="tea"></img>
         </div>
     )
 }
