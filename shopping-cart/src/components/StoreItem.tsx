6bedd0247519fccb2d7dcec062dd783cfa87e12d
xiejon <jxie77@gmail.com>
2022-08-18 13:25:24 +0200

    changed increment btns to show only when item is in cart

--- shopping-cart/src/components/StoreItem.tsx
+++ shopping-cart/src/components/StoreItem.tsx
@@ -10,56 +10,65 @@ type StoreItemProps = {
 };
 
 const StoreItem = ({ id, name, price, url }: StoreItemProps) => {
-  const { getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart } = useCart()
-  const [areBtnsShown, setAreBtnsShown] = React.useState(false)
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useCart();
+  const [areBtnsShown, setAreBtnsShown] = React.useState(false);
 
-  const quantity = getItemQuantity(id)
-  
+  const quantity = getItemQuantity(id);
+
   function showBtns() {
-    setAreBtnsShown(true)
+    setAreBtnsShown(true);
   }
 
   function hideBtns() {
-    setAreBtnsShown(false)
+    setAreBtnsShown(false);
   }
-    
+
   return (
     <div className={styles.item}>
-        <div 
-          className={styles.imgContainer}
+      <div
+        className={styles.imgContainer}
+        onMouseOver={showBtns}
+        onMouseLeave={hideBtns}
+      >
+        <img className={styles.img} src={url} alt={name}></img>
+        <div
+          className={styles.btnsContainer}
+          //
           onMouseOver={showBtns}
           onMouseLeave={hideBtns}
-          ><img 
-              className={styles.img} 
-              src={url} 
-              alt={name}
-              ></img>
-            <div 
-              className={styles.btnsContainer} 
-
-              //
-              onMouseOver={showBtns}
-              onMouseLeave={hideBtns}
-
-              style={areBtnsShown ? {"display": "flex"} : {"display": "none"}}
-              ><div className={styles.qtyBtnsContainer}>
-                <button 
-                  className={styles.decrementBtn}
-                  onClick={() => decreaseCartQuantity(id)}
-                >-</button>
-                <div className={styles.quantity}>
-                  {quantity}
-                </div>
-                <button 
-                  className={styles.incrementBtn}
-                  onClick={() => increaseCartQuantity(id)}
-                >+</button>
-              </div>
+          style={areBtnsShown ? { display: "flex" } : { display: "none" }}
+        >
+          <div className={styles.qtyBtnsContainer}>
+            {quantity === 0 ? (
               <button 
                 className={styles.addToCartBtn}
-                >Add To Cart</button>
-            </div>
+                onClick={() => increaseCartQuantity(id)}
+              >Add To Cart</button>
+            ) : (
+              <>
+                <button
+                  className={styles.decrementBtn}
+                  onClick={() => decreaseCartQuantity(id)}
+                >
+                  -
+                </button>
+                <div className={styles.quantity}>{quantity}</div>
+                <button
+                  className={styles.incrementBtn}
+                  onClick={() => increaseCartQuantity(id)}
+                >
+                  +
+                </button>
+              </>
+            )}
+          </div>
         </div>
+      </div>
       <div className={styles.info}>
         <h4 className={styles.title}>{name}</h4>
         <p className={styles.price}>${price}</p>
