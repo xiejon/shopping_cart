c33c2b4b27386150b1afd5bd1b154de75634b8c9
xiejon <jxie77@gmail.com>
2022-09-03 20:39:11 +0200

    added loading and error message

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,38 +1,43 @@
 import React from "react";
 import axios from "axios";
+import logger from "use-reducer-logger";
 import styles from "../styles/Store.module.css";
-// import storeItems from "../data/items.json";
 import StoreItem from "../components/StoreItem";
 
 const reducer = (state: any, action: any) => {
   switch (action.type) {
-    case 'FETCH_REQUEST': 
-      return { ...state, loading: true }
-    case 'FETCH_SUCCESS':
-      return { ...state, loading: false, items: action.payload }
-    case 'FETCH_FAIL':
-      return { ...state, loading: false, error: action.payload }
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, items: action.payload };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
   }
-}
+};
 
 function getErrorMessage(error: unknown) {
-  if (error instanceof Error) return error.message
-  return String(error)
+  if (error instanceof Error) return error.message;
+  return String(error);
 }
 
 const Store = () => {
-  const [{loading, error, items}, dispatch] = React.useReducer(reducer, {
-    items: [], loading: true, error: '',
-  })
+  const [{ loading, error, items }, dispatch] = React.useReducer(
+    logger(reducer),
+    {
+      items: [],
+      loading: true,
+      error: "",
+    }
+  );
 
   React.useEffect(() => {
     const fetchData = async () => {
-      dispatch({type: 'FETCH_REQUEST'})
+      dispatch({ type: "FETCH_REQUEST" });
       try {
         const result = await axios.get("/api/products");
-        dispatch({type: 'FETCH_SUCCESS', payload: result.data})
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
-        dispatch({type: 'FETCH_FAIL', payload: getErrorMessage(err) })
+        dispatch({ type: "FETCH_FAIL", payload: getErrorMessage(err) });
       }
     };
     fetchData();
@@ -46,13 +51,19 @@ const Store = () => {
         alt="tea"
       ></img>
       <div className={styles.items}>
-        {items.map((item: any) => {
-          return (
-            <div className={styles.itemContainer} key={item.id}>
-              <StoreItem {...item}></StoreItem>
-            </div>
-          );
-        })}
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          items.map((item: any) => {
+            return (
+              <div className={styles.itemContainer} key={item.id}>
+                <StoreItem {...item}></StoreItem>
+              </div>
+            );
+          })
+        )}
       </div>
     </div>
   );
