01bf2d151670aa3034cd0c9ba493c42d2675c979
xiejon <jxie77@gmail.com>
2022-09-03 19:50:37 +0200

    refactored Store.tsx to get items via axios call

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
-import express from 'express';
-import data from './data.js'
+import express from "express";
+import data from "./data.js";
 
 const app = express();
 
-app.get('/api/products', (req, res) => {
-    res.send(data.products)
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
 });
 
 const port = process.env.PORT || 8000;
+
 app.listen(port, () => {
-    console.log(`serve at http://localhost:${port}`)
-})
+  console.log(`serve at http://localhost:${port}`);
+});
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import axios from "axios";
 import styles from "../styles/Store.module.css";
-import storeItems from "../data/items.json";
+// import storeItems from "../data/items.json";
 import StoreItem from "../components/StoreItem";
 
+type Item = {
+  id: number;
+  name: string;
+  price: number;
+  url: string;
+};
+
 const Store = () => {
+  const [items, setItems] = React.useState<Item[]>([]);
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("/api/products");
+      setItems(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.container}>
       <img
@@ -12,7 +29,7 @@ const Store = () => {
         alt="tea"
       ></img>
       <div className={styles.items}>
-        {storeItems.map((item) => {
+        {items.map((item) => {
           return (
             <div className={styles.itemContainer} key={item.id}>
               <StoreItem {...item}></StoreItem>
