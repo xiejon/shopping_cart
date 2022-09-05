da93213ecd96bff9c41716ceb9b917b93db63246
xiejon <jxie77@gmail.com>
2022-09-05 17:35:59 +0200

    added sign in authentication

--- backend/data.js
+++ backend/data.js
@@ -1,36 +1,52 @@
-const data = {
-    products: [
-        {
-            "name": "Signature Pu'er",
-            "price": 13,
-            "url": "imgs/signature-puer.jpg"
-        },
-        {
-            "name": "Green Sanctuary Roast",
-            "price": 10,
-            "url": "imgs/green-sanctuary-roast.jpg"
-        },
-        {
-            "name": "Buddha's Vert",
-            "price": 12,
-            "url": "imgs/buddha-vert.jpg"
-        },
-        {
-            "name": "Dragon's Back",
-            "price": 10,
-            "url": "imgs/dragon-back.jpg"
-        },
-        {
-            "name": "Wild Chrysanthemum",
-            "price": 11,
-            "url": "imgs/wild-chrysanthemum.jpg"
-        },
-        {
-            "name": "Kunlu Wild",
-            "price": 14,
-            "url": "imgs/kunlu-wild.jpg"
-        }
-    ]
-}
+import bcrypt from "bcryptjs";
 
-export default data
+const data = {
+  users: [
+    {
+      name: "Jonathan",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: true
+    },
+    {
+      name: "Bob",
+      email: "bob@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: false
+    },
+  ],
+  products: [
+    {
+      name: "Signature Pu'er",
+      price: 13,
+      url: "imgs/signature-puer.jpg",
+    },
+    {
+      name: "Green Sanctuary Roast",
+      price: 10,
+      url: "imgs/green-sanctuary-roast.jpg",
+    },
+    {
+      name: "Buddha's Vert",
+      price: 12,
+      url: "imgs/buddha-vert.jpg",
+    },
+    {
+      name: "Dragon's Back",
+      price: 10,
+      url: "imgs/dragon-back.jpg",
+    },
+    {
+      name: "Wild Chrysanthemum",
+      price: 11,
+      url: "imgs/wild-chrysanthemum.jpg",
+    },
+    {
+      name: "Kunlu Wild",
+      price: 14,
+      url: "imgs/kunlu-wild.jpg",
+    },
+  ],
+};
+
+export default data;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -17,11 +18,18 @@ mongoose
 
 const app = express();
 
-app.use("/api/seed", seedRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
 
 const port = process.env.PORT || 8000;
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
