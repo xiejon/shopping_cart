b35e56b64c13de3a8aa04c1a4ca0c77a2f598739
xiejon <jxie77@gmail.com>
2022-09-08 20:27:45 +0200

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -60,9 +60,9 @@ app.post("/create-payment-intent", async (req, res) => {
 });
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/build")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/build/index.html"));
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
 });
 
 const port = process.env.PORT || 8000;
