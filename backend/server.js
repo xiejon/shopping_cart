61fdb205e32286705b9a6c165b55f3096b066e0b
xiejon <jxie77@gmail.com>
2022-09-08 21:30:02 +0200

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -65,7 +65,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
